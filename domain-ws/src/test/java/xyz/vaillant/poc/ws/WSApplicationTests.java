c1d3ab45a47fc51f2bb8a5db8693487b14a5eb08
craftmusic <michel.stegmiller3@gmail.com>
2023-07-08 17:40:48 +0200

    Changement dependance CI Core

--- domain-ws/src/test/java/xyz/vaillant/poc/ws/WSApplicationTests.java
+++ domain-ws/src/test/java/xyz/vaillant/poc/ws/WSApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
-import xyz.vaillant.poc.core.config.CoreConfig;
+import xyz.vaillant.poc.back.core.config.CoreConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
